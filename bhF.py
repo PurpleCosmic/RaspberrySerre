89d8e00b12f2bd2f8e43b67278e71dcc325855df
Elias Verhelst <eliasverhelst7@gmail.com>
2023-11-17 12:09:34 +0100

    Fixed graphData.py & bhF.py

--- bhF.py
+++ bhF.py
@@ -11,7 +11,7 @@ def convertToNumber(data):
 	result=(data[1] - (256 * data[0])) / 1.2
 	return result
 
-def readLight(addr=0x23)
-	data = random.random()*200
-	jsonFuncs.storeData("lightLevelsT.JSON", {"time": time.time(), "lightLevel":convertToNumber(data)})
+def readLight(addr=0x23):
+	data = [random.random()*200, random.random()*200]
+	jsonFuncs.storeData("lightLevels.JSON", {"time": time.time(), "lightLevel":convertToNumber(data)})
 	return convertToNumber(data)
--- graphData.py
+++ graphData.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 import jsonFuncs
+import datetime
 
 plt.grid(True)
 
 def plotTimedData(file, dataKey, dataLabel, color):
 	data = jsonFuncs.readFile(file)
 
-	xAxis = [key["time"] for key in data]
+	xAxis = ["{:02d}:{:02d}".format(datetime.datetime.fromtimestamp(key["time"]).hour, datetime.datetime.fromtimestamp(key["time"]).minute) for key in data]
 	yAxis = [key[dataKey] for key in data]
 
 	plt.plot(xAxis, yAxis, color = color, marker = 'o')
