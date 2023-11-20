d565a88734356df262a3acf96aa32d63b26d6e47
Elias Verhelst <64976324+PurpleCosmic@users.noreply.github.com>
2023-11-20 15:13:16 +0100

    Fix bug bhF.py

--- bhF.py
+++ bhF.py
@@ -8,7 +8,7 @@ def convertToNumber(data):
 	result=(data[1] - (256 * data[0])) / 1.2
 	return result
 
-def readLight(addr=0x23)
+def readLight(addr=0x23):
 	data = bus.read_i2c_block_data(addr,0x10)
 	jsonFuncs.storeData("lightLevels.JSON", {"time": time.time(), "lightLevel":convertToNumber(data)})
 	return convertToNumber(data)
