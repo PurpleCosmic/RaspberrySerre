966fcb278e7aa2b8a156100e14c8f9224a3b19dd
Elias Verhelst <eliasverhelst7@gmail.com>
2023-11-20 14:41:43 +0100

    Clean up time labels when graphing

--- graphData.py
+++ graphData.py
@@ -4,11 +4,29 @@ import datetime
 
 plt.grid(True)
 
+X_COUNT = 12
+
+def interpolateTimeValues(arr):
+	minVal = min(arr)
+	maxVal = max(arr)
+	valArr = []
+	diff = (maxVal-minVal)/(X_COUNT)
+
+	for i in range(X_COUNT+1):
+		valArr.append(minVal + i*diff)
+
+	return valArr
+
 def plotTimedData(file, dataKey, dataLabel, color):
 	data = jsonFuncs.readFile(file)
 
-	xAxis = ["{:02d}:{:02d}".format(datetime.datetime.fromtimestamp(key["time"]).hour, datetime.datetime.fromtimestamp(key["time"]).minute) for key in data]
+	xAxis = [key["time"] for key in data]
 	yAxis = [key[dataKey] for key in data]
+
+	valArray = interpolateTimeValues(xAxis)
+
+	timeValues = ["{:02d}:{:02d}".format(datetime.datetime.fromtimestamp(key).hour, datetime.datetime.fromtimestamp(key).minute) for key in valArray]
+	plt.xticks(valArray, timeValues)
 
 	plt.plot(xAxis, yAxis, color = color, marker = 'o')
 	plt.xlabel('time')
